3bd926fd3fa23ec7662e7d0d76562f47e539cb63
JatinBrandshow <jatin.yadav@brandshow.in>
2025-10-07 14:56:23 +0530

    first commit

---
+++ src/app/HomePage.js
@@ -0,0 +1,15 @@
+import About from "@/components/About";
+import HeroSection from "@/components/HeroSection";
+import Navbar from "@/components/NavBar";
+import React from "react";
+
+const HomePage = () => {
+    return (
+        <div>
+            <HeroSection />
+            <About/>
+        </div>
+    );
+};
+
+export default HomePage;
